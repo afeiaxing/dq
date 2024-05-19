81f020eaf55ff756fc2a78979c1b0f690aeba7ba
aaxing <zhangqiucn@gmail.com>
2024-05-19 21:12:28 +0800

    Update 新增赛事列表设置页面

--- QYZY/Module/Match/MatchList/VC/AXMatchSettingViewController.m
+++ QYZY/Module/Match/MatchList/VC/AXMatchSettingViewController.m
@@ -6,8 +6,12 @@
 //
 
 #import "AXMatchSettingViewController.h"
+#import "AXMatchListSettingCell.h"
 
-@interface AXMatchSettingViewController ()
+@interface AXMatchSettingViewController ()<UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) NSArray *settingsData;
 
 @end
 
@@ -17,17 +21,68 @@
     [super viewDidLoad];
     
     self.fd_prefersNavigationBarHidden = YES;
-    self.view.backgroundColor = UIColor.purpleColor;
+    [self.view addSubview:self.tableView];
+    [self.tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.offset(0);
+    }];
+    
+    self.settingsData = @[@{@"title": @"Matches display", @"options": @[]},
+                               @{@"title": @"Match rankings", @"options": @[@{@"title": @"", @"type": [NSNumber numberWithInt:SettingTypeSwitch]}]},
+                               @{@"title": @"Notification sound in APP", @"options": @[]},
+                               @{@"title": @"Favorites event push", @"options": @[@{@"title": @"ALL Matches", @"type": [NSNumber numberWithInt:SettingTypeArrow]}]},
+                               @{@"title": @"Score Notification", @"options": @[]},
+                               @{@"title": @"Voice notification", @"options": @[@{@"title": @"", @"type": [NSNumber numberWithInt:SettingTypeSwitch]}]},
+                               @{@"title": @"Vibrate", @"options": @[@{@"title": @"", @"type": [NSNumber numberWithInt:SettingTypeSwitch]}]},
+                               @{@"title": @"Mobile phone notification bar push settings", @"options": @[]},
+                               @{@"title": @"Notification bar push", @"options": @[@{@"title": @"", @"type": [NSNumber numberWithInt:SettingTypeSwitch]}]},
+                               @{@"title": @"Favorites event push", @"options": @[@{@"title": @"3/3", @"type": [NSNumber numberWithInt:SettingTypeArrow]}]}];
+        
+        [self.tableView registerClass:[AXMatchListSettingCell class] forCellReuseIdentifier:@"SettingCell"];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
 }
-*/
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.settingsData.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    AXMatchListSettingCell *cell = [tableView dequeueReusableCellWithIdentifier:NSStringFromClass(AXMatchListSettingCell.class) forIndexPath:indexPath];
+    
+    NSDictionary *setting = self.settingsData[indexPath.row];
+    cell.title = setting[@"title"];
+    
+    NSArray *options = setting[@"options"];
+    if (options.count > 0) {
+        NSDictionary *option = options[0];
+        NSNumber *typeNum = option[@"type"];
+        SettingType type = (SettingType)(typeNum.intValue);
+        switch (type) {
+            case SettingTypeSwitch:
+                cell.accessoryView = [[UISwitch alloc] init];
+                break;
+            case SettingTypeArrow:
+                cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+                break;
+            default:
+                break;
+        }
+    }
+    
+    return cell;
+}
+
+// MARK: setter & getter
+- (UITableView *)tableView{
+    if (!_tableView) {
+        _tableView = [UITableView new];
+        [_tableView registerClass:AXMatchListSettingCell.class forCellReuseIdentifier:NSStringFromClass(AXMatchListSettingCell.class)];
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+    }
+    return _tableView;
+}
 
 @end
